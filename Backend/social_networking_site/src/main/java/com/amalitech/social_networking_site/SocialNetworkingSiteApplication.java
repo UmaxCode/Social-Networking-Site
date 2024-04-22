08905302c9798917a35896a995ee5601f167d12b
umaxcode <maxwellodoom1729@gmail.com>
2024-04-22 08:38:51 +0100

    initial setup with the base dependencies (frontend and backend)

---
+++ Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/SocialNetworkingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.social_networking_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkingSiteApplication.class, args);
+	}
+
+}
---
+++ Frontend/social_networking_site/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="">Frontend</div>;
+}
+
+export default App;
