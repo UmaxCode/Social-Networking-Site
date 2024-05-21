e67c421aab1685a1e3ba403b78537a22ea07852c
UmaxCode <123482759+UmaxCode@users.noreply.github.com>
2024-05-21 02:23:50 +0000

    Update AuthWrapper.tsx for successful build

--- Frontend/social_networking_site/src/contexts/AuthWrapper.tsx
+++ Frontend/social_networking_site/src/contexts/AuthWrapper.tsx
@@ -14,13 +14,13 @@ type ContextValue = {
 };
 
 const initialContextValue: ContextValue = {
-  login: function (token: string): void {},
+  login: function (token: string): void {console.log(token)},
   logout: function (): void {},
   authenticate: {
     token: null,
     isAuthenticated: false,
   },
-  setProfilePic: function (url: string): void {},
+  setProfilePic: function (url: string): void {console.log(token)},
   profile: null,
 };
 
