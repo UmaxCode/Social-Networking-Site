c08cbdd5a805c881063cfd7835e0edde8714baa8
umaxcode <maxwellodoom1729@gmail.com>
2024-10-21 15:48:42 +0000

    refactor(backend): update application config

--- Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/configs/AppConfig.java
+++ Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/configs/AppConfig.java
@@ -1,20 +1,10 @@
 package com.amalitech.social_networking_site.configs;
 
-import com.amalitech.social_networking_site.entities.userDetials.UserDetailsImpl;
-import com.amalitech.social_networking_site.repositories.UserRepository;
 import com.cloudinary.Cloudinary;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,49 +13,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AppConfig {
 
-
     @Value("${cloudinary.name}")
     private String cloud_name;
-
     @Value("${cloudinary.key}")
     private String cloud_api_key;
-
     @Value("${cloudinary.secretKey}")
     private String cloud_secret_key;
 
 
-    private final UserRepository userRepository;
-
-
     @Bean
-    public PasswordEncoder hashPassword() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
-        daoAuthenticationProvider.setPasswordEncoder(hashPassword());
-
-        return daoAuthenticationProvider;
-
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-
-        return username -> userRepository.findByEmail(username).map(user -> UserDetailsImpl.builder().email(user.getEmail()).password(user.getPassword()).role(user.getRole()).build()).orElseThrow(()-> new IllegalArgumentException("User does not exit"));
-
-    }
-
-    @Bean
-    public Cloudinary getCloudinary(){
+    public Cloudinary getCloudinary() {
 
         Map<String, String> config = new HashMap<>();
 
@@ -74,9 +31,6 @@ public class AppConfig {
         config.put("api_secret", cloud_secret_key);
 
         return new Cloudinary(config);
-
-
-
 
     }
 
