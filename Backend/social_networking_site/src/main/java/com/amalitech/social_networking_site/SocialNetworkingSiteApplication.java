89413755875987a9d9daed424bf69438cbe1b9e4
umaxcode <maxwellodoom1729@gmail.com>
2024-10-21 15:46:27 +0000

    refactor(backend): update the application entry point

--- Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/SocialNetworkingSiteApplication.java
+++ Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/SocialNetworkingSiteApplication.java
@@ -1,17 +1,15 @@
 package com.amalitech.social_networking_site;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SocialNetworkingSiteApplication {
 
-	public static void main(String[] args) {
-
-		Dotenv.configure().load();
-
-		SpringApplication.run(SocialNetworkingSiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkingSiteApplication.class, args);
+    }
 
 }
