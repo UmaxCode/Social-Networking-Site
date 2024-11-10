98ac3adc29184e20b298c2d76101cfa4a0139494
umaxcode <maxwellodoom1729@gmail.com>
2024-11-10 19:35:52 +0000

    refactor(backend): refactor service and code cleanup

--- Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/services/UserAuthService.java
+++ Backend/social_networking_site/src/main/java/com/amalitech/social_networking_site/services/UserAuthService.java
@@ -7,24 +7,18 @@ import com.amalitech.social_networking_site.entities.User;
 import com.amalitech.social_networking_site.entities.UserProfile;
 import com.amalitech.social_networking_site.repositories.ProfileRepository;
 import com.amalitech.social_networking_site.repositories.UserRepository;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.util.Optional;
-import java.util.UUID;
 
 import static com.amalitech.social_networking_site.utilities.Utilities.*;
 
@@ -38,66 +32,42 @@ public class UserAuthService {
     private final JWTAuthenticationService jwtAuthenticationService;
     private final EmailService emailService;
     private final AuthenticationManager authenticationManager;
-    private final JwtDecoder jwtDecoder;
 
-    @Value("${spring.security.oauth2.client.registration.google.clientId}")
-    private String clientId;
-
-    @Value("${spring.security.oauth2.client.registration.google.clientSecret}")
-    private String clientSecret;
     @Value("${frontend.url}")
     private String frontEndUrl;
 
 
     public String register(UserCreationRequest userData) throws MessagingException, IOException {
 
-        try {
-            // TODO: input validation
-            User user = User.builder()
-                    .fullName(userData.fullname())
-                    .username(userData.username())
-                    .email(userData.email())
-                    .role(Role.REG_USER)
-                    .password(passwordEncoder.encode(userData.password()))
-                    .isActive(false)
-                    .build();
+        User user = User.builder()
+                .fullName(userData.fullname())
+                .username(userData.username())
+                .email(userData.email())
+                .role(Role.REG_USER)
+                .password(passwordEncoder.encode(userData.password()))
+                .build();
 
 
-            User savedUser = userRepository.save(user);
+        User savedUser = userRepository.save(user);
 
-            var profile = UserProfile.builder()
-                    .user(savedUser)
+        var profile = UserProfile.builder()
+                .user(savedUser)
+                .build();
 
-                    .build();
+        profileRepository.save(profile);
 
-            profileRepository.save(profile);
+        String generatedToken = jwtAuthenticationService.generateToken(savedUser.getEmail(), TokenSubject.EMAIL_VERIFICATION);
 
-            String generatedToken = jwtAuthenticationService.generateToken(savedUser.getEmail(), TokenSubject.EMAIL_VERIFICATION);
+        emailService.sendMail("email Account Verification", "email_verification.html", savedUser.getEmail(), savedUser.getUsername(), generatedToken);
 
-            emailService.sendMail("email Account Verification", "email_verification.html", savedUser.getEmail(), savedUser.getUsername(), generatedToken);
+        return String.format("Account created successfully! We've sent a verification link to you email - %s", savedUser.getEmail());
 
-            return String.format("Account created successfully! We've sent a verification link to you email - %s", savedUser.getEmail());
-        } catch (DataIntegrityViolationException err) {
-            throw new IllegalArgumentException("username or email is already in use.");
-        } catch (MessagingException err) {
-            throw new IllegalArgumentException("error occurred when sending email. email server must be down.");
-        }
 
     }
 
     public UserAuthenticationResponse authenticate(UserAuthenticationRequest userData) {
 
-        User user = userRepository.findByEmail(userData.email()).orElseThrow(() -> new IllegalArgumentException("user does not exist. Sign up now!"));
-
-        if (!user.getIsActive()) {
-            throw new IllegalArgumentException("Your account is not verified");
-        }
-
-        if (user.getPassword() == null) {
-            throw new IllegalArgumentException("Your haven't set a password for your account. Click forgot password link to set it now!");
-        }
-
-        authenticationManager.authenticate(
+        Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         userData.email(),
                         userData.password()
@@ -106,85 +76,14 @@ public class UserAuthService {
 
         String generatedToken = jwtAuthenticationService.generateToken(userData.email(), TokenSubject.LOGIN);
 
+        User user = (User) authentication.getPrincipal();
 
-        return new UserAuthenticationResponse("You are logged in", generatedToken, user.getUsername(), user.getProfile().getFilePath());
-    }
-
-
-    private UserAuthenticationResponse registerOauthUser (String token){
-
-
-       var  claims =  jwtDecoder.decode(token);
-
-       var body = claims.getClaims();
-
-        String email = String.valueOf(body.get("email"));
-
-        String firstname = String.valueOf(body.get("given_name"));
-
-        String lastname = String.valueOf(body.get("family_name"));
-
-//        String picture = String.valueOf(body.get("picture"));
-
-
-        Optional<User> optionalUser = userRepository.findByEmail(email);
-
-        User savedUser;
-
-        if (optionalUser.isEmpty()) {
-
-            User user = User.builder()
-                    .email(email)
-                    .fullName(firstname + " " + lastname)
-                    .isActive(true)
-                    .username(firstname + (UUID.randomUUID()).toString().split("-")[0])
-                    .role(Role.REG_USER)
-
-                    .build();
-            savedUser = userRepository.save(user);
-
-            var profile = UserProfile.builder()
-                    .user(savedUser)
-                    .onlineStatus(false)
-                    .build();
-
-            profileRepository.save(profile);
-
-            savedUser.setProfile(profile);
-
-            userRepository.save(savedUser);
-
-
-        }else {
-            savedUser = optionalUser.get();
-        }
-
-        String generatedToken = jwtAuthenticationService.generateToken(email, TokenSubject.LOGIN);
-
-        return new UserAuthenticationResponse("Access granted. You're now logged in.", generatedToken, savedUser.getUsername(), savedUser.getProfile().getFilePath());
-
-    }
-
-
-    public UserAuthenticationResponse handleGoogleCallback(String code) {
-
-        try {
-            String token = new GoogleAuthorizationCodeTokenRequest(
-                    new NetHttpTransport(), new GsonFactory(),
-                    clientId,
-                    clientSecret,
-                    code,
-                    frontEndUrl
-            ).execute().getIdToken();
-
-
-            return registerOauthUser(token);
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Google OAuth failed", e);
-        }
-
+        return UserAuthenticationResponse.builder()
+                .message("You are logged in")
+                .token(generatedToken)
+                .username(user.getUsername())
+                .profile_pic(user.getProfile().getFilePath())
+                .build();
     }
 
 
@@ -213,7 +112,7 @@ public class UserAuthService {
 
     }
 
-    public void emailVerification(String token) {
+    public RedirectView emailVerification(String token) {
 
         if (jwtAuthenticationService.isValidToken(token, TokenSubject.EMAIL_VERIFICATION)) {
 
@@ -221,7 +120,10 @@ public class UserAuthService {
 
             user.setIsActive(true);
             userRepository.save(user);
-            return;
+
+            RedirectView redirectView = new RedirectView();
+            redirectView.setUrl(frontEndUrl);
+            return redirectView;
         }
 
         throw new IllegalArgumentException("wrong verification token");
